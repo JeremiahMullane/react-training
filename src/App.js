f66b40c1ef0a66706eeb2bd06a3756c89efa1274
Jeremiah Mullane <jmullane@athenahealth.com>
2020-01-13 13:06:01 -0500

    use state hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([
+    //users now has a hook to update state, called setUsers. It is a "state variable"
+    { id: 1, name: "Cory", email: "c@h.com" },
+    { id: 2, name: "Megan", email: "m@c.com" },
+    { id: 3, name: "Tami", email: "t@tonga.com" }
+  ]);
+
   const h1Style = {
     color: "red",
     marginBottom: 20
   };
 
-  const users = [
-    { id: 1, name: "Cory", email: "c@h.com" },
-    { id: 1, name: "Megan", email: "m@c.com" },
-    { id: 1, name: "Tami", email: "t@tonga.com" }
-  ];
+  function handleDelete(id) {
+    // Remove deleted element from users array
+    const newUsers = users.filter(user => user.id !== id);
+    setUsers(newUsers); //update state, so react knows to re-render
+  }
 
   return (
     <>
       <h1 className="header" style={h1Style}>
-        App
+        Users
       </h1>
+      <ul>
+        {users.map(user => (
+          <li>
+            {/* Delay execution via arrow function */}
+            <button onClick={() => handleDelete(user.id)}>Delete</button>
+            {user.name}
+          </li>
+        ))}
+      </ul>
       <label htmlFor="firstName">First Name </label>
       <input id="firstName" type="text"></input>
       <p>My app.</p>
