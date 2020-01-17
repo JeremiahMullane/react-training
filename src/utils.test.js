db76d9b71d54000eb945434fb4659d243f92ce11
Jeremiah Mullane <jmullane@athenahealth.com>
2020-01-17 11:18:40 -0500

    add first test

--- src/utils.test.js
+++ src/utils.test.js
@@ -3,7 +3,11 @@ import { isValidEmail } from "./utils";
 describe("utils", () => {
   it("should return true when passed c@h.com", () => {
     //arrange
+
     //act
+    const isValid = isValidEmail("c@h.com");
+
     //assert
+    expect(isValid).toBe(true);
   });
 });
